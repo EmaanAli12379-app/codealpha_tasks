4f69a0cb922111e88d95e925edc796e801d4f575
EmaanAli12379-app <emaanali12379@gmail.com>
2026-07-30 16:00:53 -0700

    Add files via upload

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3109833531" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3390447173" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
